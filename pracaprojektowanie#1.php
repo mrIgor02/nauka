987c0f0228e10cda6b4f824ae9aeed0ddbf460b3
mrIgor02 <igorwieszczynski1@gmail.com>
2026-02-11 11:28:44 +0100

    pracaprojektowanie#1

---
+++ pracaprojektowanie#1.php
@@ -0,0 +1,41 @@
+<?php
+
+
+ for($i=0;$i<10;$i++)
+    {
+        echo "i= ".$i."<br>";
+    }
+
+
+
+
+    echo"<p>pętla WHILE</p>";
+    $j=0;
+
+    while($j<10)
+        {
+            echo "j= ".$j."<br>";
+            $j++;
+        }
+
+
+        $k=0;
+        do
+        {
+            echo "k= ".$k."<br>";
+            $k++;
+        }
+    while($k<10);
+
+    $m=1;
+    $suma=0;
+    while($m<=100)
+        {
+            $suma++$m;
+            $m++;
+        }
+        echo"wynik = ".$suma;
+    
+
+
+?> 
